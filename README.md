f4f6078a32d398d9d7dd2731c6026ad093a83e25
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-12-05 11:37:10 +0100

    push back with crossOrigin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # top-collegues-back
+
+Lien heroku de l'application : https://matthieu-top-collegues.herokuapp.com/versions 
--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import dev.top.entities.Collegue;
 import dev.top.repos.CollegueRepo;
 import dev.top.service.ChampAvis;
 
+
+@CrossOrigin
 @RestController // => pas besoin si @ResponseBody sur Méthode.
 @RequestMapping("/collegues")
 public class CollegueCtrl {
